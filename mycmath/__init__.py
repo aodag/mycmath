f644f862abf06a685d2e1763dc2b754315447992
Atsushi Odagiri <aodagx@gmail.com>
2018-05-21 08:12:24 +0900

    use setuptools_scm

--- mycmath/__init__.py
+++ mycmath/__init__.py
@@ -1,5 +1,7 @@
-__version__ = '0.1.1'
+from _mycmath import add  # NOQA
+from pkg_resources import get_distribution, DistributionNotFound
 try:
-    from _mycmath import add  # NOQA
-except ImportError:
+    __version__ = get_distribution(__name__).version
+except DistributionNotFound:
+    # package is not installed
     pass
--- setup.py
+++ setup.py
@@ -4,5 +4,7 @@ from setuptools import setup, Extension
 mod = Extension("_mycmath",
                 sources=["mycmathmodule.c"])
 setup(
+    setup_requires=["setuptools_scm"],
+    use_scm_version=True,
     ext_modules=[mod],
 )
